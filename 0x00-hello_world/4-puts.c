149db485820cb84af3c7faa686ba61f8b6fc656c
Arohby <babarindeabdroheem2000@gmail.com>
2022-09-08 22:31:21 +0100

    Task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  *
  *  * main - Entry point
@@ -9,9 +8,7 @@
  *    * Return: 0 (success)
  *
  *     */
-
 int main(void)
-
 {
      puts("\"Programming is like building a multilingual puzzle");
 		return (0);
