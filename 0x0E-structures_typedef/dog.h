7ca521946001455ba3843756d18acf75b9b1e6f0
Arohby <babarindeabdroheem2000@gmail.com>
2022-10-11 06:04:06 +0100

    Task

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,13 @@
-#ifndef _HEADER_
-#define _HEADER_
+#ifndef _DOG_H_
+#define _DOG_H_
 
 /**
- * struct dog - Dog attributes
- * @name: The name of the dog.
- * @age: The age of the dog.
- * @owner: The owner of the dog.
+ * struct dog - struct for dog attributes
  *
- * Description: The attributes of a dog.
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner of dog
+ *
  */
 struct dog
 {
@@ -17,13 +17,13 @@ struct dog
 };
 
 /**
- * dog_t - Typedef for dog structure
+ * dog_t - typedef for struct dog
+ *
  */
 typedef struct dog dog_t;
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
-#endf
+#endif /* DOG_H */
